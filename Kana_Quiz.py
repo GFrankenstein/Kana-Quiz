06315599da6c2f8a8af947cb7d6f8fd2be70c4c4
G.Frankenstein <frankensteinyang@gmail.com>
2015-01-14 12:21:18 +0900

    changes for cross-platform
    enhanced compatibility.

--- Kana_Quiz.py
+++ Kana_Quiz.py
@@ -4,20 +4,21 @@ p=[]
 for i in range(46):
 	p.append(random.randint(0,2))
 
-r=['0a0A a','0i0I i','0u0U u','0e0E e','0o0O o','KaKAka','KiKIki','KuKUku','KeKEke','KoKOko','SaSAsa','SiSIsI','SuSUsu','SeSEse','SoSOso','TaTAta','TiTItI','TuTUtU','TeTEte','ToTOto','NaNAna','NiNIni','NuNUnu','NeNEne','NoNOno','HaHAha','HiHIhi','FuFUfu','HeHEhe','HoHOho','MaMAma','MiMImi','MuMUmu','MeMEme','MoMOmo','YaYAya','YuYUyu','YoYOyo','RaRAra','RiRIri','RuRUru','ReREre','RoROro','WaWAwa','1o1Oo*','0n0N n']
+r=['0a0A a','0i0I i','0u0U u','0e0E e','0o0O o','KaKAka','KiKIki','KuKUku','KeKEke','KoKOko','SaSAsa','SiSIsI','SuSUsu','SeSEse','SoSOso','TaTAta','TiTItI','TuTUtU','TeTEte','ToTOto','NaNAna','NiNIni','NuNUnu','NeNEne','NoNOno','HaHAha','HiHIhi','FuFUfu','HeHEhe','HoHOho','MaMAma','MiMImi','MuMUmu','MeMEme','MoMOmo','YaYAya','YuYUyu','YoYOyo','RaRAra','RiRIri','RuRUru','ReREre','RoROro','WaWAwa','WoWOwo','0n0N n']
 random.shuffle(r)
 
-print '\documentclass[UTF8]{ctexart}'
-print '\usepackage{multicol}'
+print '\documentclass{article}'
+print '\usepackage{multicol,fontspec}'
 print '\pagestyle{empty}'
 print '\\begin{document}'
+print '\setmainfont{NtMotoyaKyotaiStd-W2.otf}'
 print '\\begin{multicols}{2}'
 print '\\begin{center}'
 print '\\begin{tabular}{rccc}'
 
 for i in range(23):
 	if (p[i]==0):
-		print i+1,'&'+r[i][0:2]+'&\\framebox[6mm]{\\rule{0pt}{4mm}}&\\framebox[6mm]{\\rule{0pt}{4mm}}\\\\'
+            print i+1,'&'+r[i][0:2]+'&\\framebox[6mm]{\\rule{0pt}{4mm}}&\\framebox[6mm]{\\rule{0pt}{4mm}}\\\\'
 	else:
 		if (p[i]==1):
 			print i+1,'&\\framebox[6mm]{\\rule{0pt}{4mm}}&'+r[i][2:4]+'&\\framebox[6mm]{\\rule{0pt}{4mm}}\\\\'
